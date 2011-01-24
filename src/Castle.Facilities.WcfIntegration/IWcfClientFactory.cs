48dfd14c96dd4d5fe44b8b4f869cda496d01ea65
Krzysztof Kozmic <krzysztof@kozmic.pl>
2011-01-24 16:12:54 +1000

    - added comments to IWcfClientFactory

--- src/Castle.Facilities.WcfIntegration/IWcfClientFactory.cs
+++ src/Castle.Facilities.WcfIntegration/IWcfClientFactory.cs
@@ -16,9 +16,20 @@ namespace Castle.Facilities.WcfIntegration
 {
 	using System;
 
+	using Castle.Facilities.TypedFactory;
 	using Castle.Facilities.WcfIntegration.Client;
 	using Castle.Facilities.WcfIntegration.Model;
 
+	/// <summary>
+	///   Factory interface for pulling wcf client-side proxies on the fly from the client side.
+	/// </summary>
+	/// <remarks>
+	///   Most of the time you will take dependency in your code on the client-side proxy interface and Windsor with the facility will construct and provide a running proxy to you as your objects get constructed.
+	///   In cases when you need to obtain the proxy at some later point, and some additional input is required (like the address to connect to) not known outright, you can use this interface to pull the proxies.
+	///   The interface is automatically registered by the <see cref = "WcfFacility" /> if upon initialization it detects that  <see
+	///     cref = "TypedFactoryFacility" /> had been added to the container prior. Notice that means that the <see
+	///    cref = "TypedFactoryFacility" /> has to be added to the container first, <see cref = "WcfFacility" /> second.
+	/// </remarks>
 	public interface IWcfClientFactory
 	{
 		T GetClient<T>(string name) where T : class;
